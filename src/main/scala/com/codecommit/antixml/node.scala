e7d1876433bead051ab98dae4e694a9921268149
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-11-22 15:16:30 +0100

    Hide api from external clients

--- src/main/scala/com/codecommit/antixml/node.scala
+++ src/main/scala/com/codecommit/antixml/node.scala
@@ -232,15 +232,18 @@ case class Elem(name: QName, attrs: Attributes = Attributes(), namespaces: Names
 object Elem {
   private[this] val NameRegex = {
     val nameStartChar = """:A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD"""
-    ("[" + nameStartChar + "][" + nameStartChar + """\-\.0-9\u00B7\u0300-\u036F\u203F-\u2040]*""").r
+    val pattern = """[%s][%s\-\.0-9\u00B7\u0300-\u036F\u203F-\u2040]*""".format(nameStartChar, nameStartChar)
+    pattern.r
   }
 
-  def apply(name: QName):Elem = apply(name, Attributes(), NamespaceBinding.empty, Group.empty)
+  def apply(name: QName, attrs: Attributes):Elem = apply(name, attrs, NamespaceBinding.empty, Group.empty)
 
-  def isValidName(string: String) = NameRegex.pattern.matcher(string).matches
-  def isValidNamespaceUri(uri: String) = uri.trim().length() > 0
+  def apply(name: QName):Elem = apply(name, Attributes())
 
-  def validateAttributes(attrs: Attributes, namespaces: List[NamespaceBinding]) {
+  private [antixml] def isValidName(string: String) = NameRegex.pattern.matcher(string).matches
+  private [antixml] def isValidNamespaceUri(uri: String) = uri.trim().length() > 0
+
+  private [antixml] def validateAttributes(attrs: Attributes, namespaces: List[NamespaceBinding]) {
     attrs.keys.foreach(name => if (!namespaces.contains(name.namespace)) throw new IllegalArgumentException("Attribute with name '%s' in '%s' namespace is not defined on element".format(name.name, name.namespace.uri.getOrElse(""))))
   }
 }
